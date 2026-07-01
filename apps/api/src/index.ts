3bb21f8f73a9bc8dbb3d3617e5d462ff355f719e
SinaSalimirad <SinaSalimirad@proton.me>
2026-07-01 10:06:51 +0330

    feat: add Logixlysia logger

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import { logger } from "./plugins/logger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(4000);
+const app = new Elysia()
+	.use(logger("api"))
+	.get("/", () => "Hello World")
+	.listen(4000);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
